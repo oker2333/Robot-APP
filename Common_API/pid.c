e2ef4e8ed3c3085c73bf6a6877c685b66918c642
2023181377@qq.com <joker>
2022-11-03 22:14:54 +0800

    [功能]PID参数优化V 2.0

--- Common_API/pid.c
+++ Common_API/pid.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <limits.h>
 
-#define MAXOUT 1000                 //输出最大值
+#define MAXOUT 1000                  //输出最大值
 #define MINOUT -1000                 //输出最小值
 
 PID sPID;
@@ -12,14 +12,14 @@ static pPID sptr;
 void IncPIDInit(void)
 {
     sptr                = &sPID;
-    sptr->SetPoint      = 0;      //设定值
+    sptr->SetPoint      = 0;      	//设定值
     sptr->BitMove       = 0u;       //返回结果比例
 
     sptr->LastError     = 0u;       //前2次误差值
     sptr->PrevError     = 0u;       //前1次误差值
 
-    sptr->Proportion    = 0.325f;     //比例
-    sptr->Integral      = 0.102f;     //积分
+    sptr->Proportion    = 0.325f;   //比例
+    sptr->Integral      = 0.102f;   //积分
     sptr->Derivative    = 0.0f;     //微分
 
     sptr->iError        = 0;        //当前误差
@@ -34,7 +34,7 @@ int IncPIDCalc(int SetPoint,int measurement)
 	  
     //当前误差
     sptr->iError = sptr->SetPoint - measurement;
-	  if((sptr->iError <= 5) && (sptr->iError >= -5))
+	  if((sptr->iError <= 8) && (sptr->iError >= -8))
 		{
 			 return INT_MAX;
 		}
