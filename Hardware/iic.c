5de6ac5473c1467ff07ae6adf472a7671c3e69ff
2023181377@qq.com <joker>
2022-11-23 23:20:12 +0800

    [功能]软件IIC(待验证)

--- Hardware/iic.c
+++ Hardware/iic.c
@@ -1,5 +1,7 @@
 #include "iic.h"
 
+/*************************************************Hardware IIC*************************************************/
+
 #define POLLING_COUNT 1000
 static uint32_t delay_counts = 0;
 
@@ -262,3 +264,628 @@ uint8_t VL6180_Read_Multiple_Register(I2C_TypeDef I2Cx, uint8_t SlaveAddress, ui
 		
 		return 0;
 }
+/********************************************delay*****************************************************************/
+/**
+ * @defgroup delay delay function
+ * @brief    delay function modules
+ * @{
+ */
+
+/**
+ * @brief  delay clock init
+ * @return status code
+ *         - 0 success
+ * @note   none
+ */
+uint8_t delay_init(void)
+{
+	 
+}
+
+/**
+ * @brief     delay us
+ * @param[in] us
+ * @note      none
+ */
+void delay_us(uint32_t us)
+{
+	 
+}
+
+/**
+ * @brief     delay ms
+ * @param[in] ms
+ * @note      none
+ */
+void delay_ms(uint32_t ms)
+{
+	 
+}
+
+/*************************************************Software IIC*************************************************/
+
+/**
+ * @brief bit operate definition
+ */
+#define BITBAND(addr, bitnum)    ((addr & 0xF0000000) + 0x2000000 + ((addr & 0xFFFFF) << 5) + (bitnum << 2)) 
+#define MEM_ADDR(addr)           *((unsigned long *)(addr)) 
+#define BIT_ADDR(addr, bitnum)   MEM_ADDR(BITBAND(addr, bitnum))
+
+/**
+ * @brief iic gpio operate definition
+ */
+
+#define SCL_IN()          {MEM_ADDR(GPIOE+0x00) &= ~(3 << (7 * 4)); MEM_ADDR(GPIOE+0x00) |= 0 << 7 * 4;}
+#define SCL_OUT()         {MEM_ADDR(GPIOE+0x00) &= ~(3 << (7 * 4)); MEM_ADDR(GPIOE+0x00) |= 1 << 7 * 4;}
+#define SDA_IN()          {MEM_ADDR(GPIOE+0x04) &= ~(3 << (0 * 4)); MEM_ADDR(GPIOE+0x04) |= 0 << 0 * 4;}
+#define SDA_OUT()         {MEM_ADDR(GPIOE+0x04) &= ~(3 << (0 * 4)); MEM_ADDR(GPIOE+0x04) |= 1 << 0 * 4;}
+#define IIC_SCL           BIT_ADDR(GPIOE+0x0C, 7)
+#define IIC_SDA           BIT_ADDR(GPIOE+0x0C, 8)
+#define READ_SCL          BIT_ADDR(GPIOE+0x08, 7)
+#define READ_SDA          BIT_ADDR(GPIOE+0x08, 8)
+
+
+/**
+ * @brief  iic bus init
+ * @return status code
+ *         - 0 success
+ * @note   SCL is PE7 and SDA is PE8
+ */
+uint8_t software_iic_init(void)
+{
+		rcu_periph_clock_enable(RCU_GPIOE);
+	  rcu_periph_clock_enable(RCU_AF);
+	  
+		gpio_init(GPIOE, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ,GPIO_PIN_7 | GPIO_PIN_8);
+    
+    IIC_SDA = 1;
+    IIC_SCL = 1;
+	  
+    return 0;
+}
+
+/**
+ * @brief  iic bus deinit
+ * @return status code
+ *         - 0 success
+ * @note   SCL is PE7 and SDA is PE8
+ */
+uint8_t software_iic_deinit(void)
+{
+    
+    IIC_SDA = 1;
+    IIC_SCL = 1;
+	  
+    return 0;
+}
+
+/**
+ * @brief  iic bus send start
+ * @note   none
+ */
+static void _iic_start(void)
+{
+    SDA_OUT();
+    IIC_SDA = 1;
+    IIC_SCL = 1;
+    delay_us(4);
+    IIC_SDA = 0;
+    delay_us(4);
+    IIC_SCL = 0;
+}
+
+/**
+ * @brief  iic bus send stop
+ * @note   none
+ */
+static void _iic_stop(void)
+{
+    SDA_OUT();
+    IIC_SCL = 0;
+    IIC_SDA = 0;
+    delay_us(4);
+    IIC_SCL = 1;
+    delay_us(4);
+    IIC_SDA = 1;
+    delay_us(4);
+}
+
+/**
+ * @brief  iic wait ack
+ * @return status code
+ *         - 0 get ack
+ *         - 1 no ack
+ * @note   none
+ */
+static uint8_t _iic_wait_ack(void)
+{
+    uint16_t uc_err_time = 0;
+    
+    SDA_IN();
+    IIC_SDA = 1; 
+    delay_us(1);
+    IIC_SCL = 1; 
+    delay_us(1);
+    while (READ_SDA)
+    {
+        uc_err_time++;
+        if (uc_err_time > 250)
+        {
+            _iic_stop();
+            
+            return 1;
+        }
+    }
+    IIC_SCL = 0;
+   
+    return 0;
+}
+
+/**
+ * @brief  iic bus send ack
+ * @note   none
+ */
+static void _iic_ack(void)
+{
+    IIC_SCL = 0;
+    SDA_OUT();
+    IIC_SDA = 0;
+    delay_us(2);
+    IIC_SCL = 1;
+    delay_us(2);
+    IIC_SCL = 0;
+}
+
+/**
+ * @brief  iic bus send nack
+ * @note   none
+ */
+static void _iic_nack(void)
+{
+    IIC_SCL = 0;
+    SDA_OUT();
+    IIC_SDA = 1;
+    delay_us(2);
+    IIC_SCL = 1;
+    delay_us(2);
+    IIC_SCL = 0; 
+}
+
+/**
+ * @brief     iic send one byte
+ * @param[in] txd send byte
+ * @note      none
+ */
+static void _iic_send_byte(uint8_t txd)
+{
+    uint8_t t;   
+    
+    SDA_OUT();
+    IIC_SCL = 0;
+    for (t=0; t<8; t++)
+    {
+        IIC_SDA = (txd&0x80) >> 7;
+        txd <<= 1;
+        delay_us(2);
+        IIC_SCL = 1;
+        delay_us(2);
+        IIC_SCL = 0;
+        delay_us(2);
+    }
+}
+
+/**
+ * @brief     iic read one byte
+ * @param[in] ack is whether to send ack
+ * @return    read byte
+ * @note      none
+ */
+static uint8_t _iic_read_byte(uint8_t ack)
+{
+    uint8_t i,receive = 0;
+    
+    SDA_IN();
+    for (i=0; i<8; i++)
+    {
+        IIC_SCL = 0;
+        delay_us(2);
+        IIC_SCL = 1;
+        receive <<= 1;
+        if (READ_SDA)
+        {
+            receive++;
+        }
+        delay_us(1);
+    }
+    if (!ack)
+    {
+        _iic_nack();
+    }
+    else
+    {
+        _iic_ack();
+    }
+    
+    return receive;
+}
+
+/**
+ * @brief     iic bus write command
+ * @param[in] addr is iic device write address
+ * @param[in] *buf points to a data buffer
+ * @param[in] len is the length of the data buffer
+ * @return    status code
+ *            - 0 success
+ *            - 1 write failed
+ * @note      SCL is PE7 and SDA is PE8
+ */
+uint8_t iic_write_cmd(uint8_t addr, uint8_t *buf, uint16_t len)
+{
+    uint16_t i; 
+    
+    _iic_start();
+    _iic_send_byte(addr);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    for (i=0; i<len; i++)
+    {
+        _iic_send_byte(buf[i]);
+        if (_iic_wait_ack())
+        {
+            _iic_stop();
+            
+            return 1;
+        }
+    }
+    _iic_stop();
+    
+    return 0;
+} 
+
+/**
+ * @brief     iic bus write
+ * @param[in] addr is iic device write address
+ * @param[in] reg is iic register address
+ * @param[in] *buf points to a data buffer
+ * @param[in] len is the length of the data buffer
+ * @return    status code
+ *            - 0 success
+ *            - 1 write failed
+ * @note      SCL is PE7 and SDA is PE8
+ */
+uint8_t iic_write(uint8_t addr, uint8_t reg, uint8_t *buf, uint16_t len)
+{
+    uint16_t i; 
+  
+    _iic_start();
+    _iic_send_byte(addr);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    _iic_send_byte(reg);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    for (i=0; i<len; i++)
+    {
+        _iic_send_byte(buf[i]);
+        if (_iic_wait_ack())
+        {
+            _iic_stop(); 
+            
+            return 1;
+        }
+    }
+    _iic_stop();
+    
+    return 0;
+} 
+
+/**
+ * @brief     iic bus write with 16 bits register address 
+ * @param[in] addr is iic device write address
+ * @param[in] reg is iic register address
+ * @param[in] *buf points to a data buffer
+ * @param[in] len is the length of the data buffer
+ * @return    status code
+ *            - 0 success
+ *            - 1 write failed
+ * @note      SCL is PE7 and SDA is PE8
+ */
+uint8_t iic_write_address16(uint8_t addr, uint16_t reg, uint8_t *buf, uint16_t len)
+{
+    uint16_t i; 
+  
+    _iic_start();
+    _iic_send_byte(addr);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    _iic_send_byte((reg>>8)&0xFF);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    _iic_send_byte(reg&0xFF);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    for (i=0; i<len; i++)
+    {
+        _iic_send_byte(buf[i]);
+        if (_iic_wait_ack())
+        {
+            _iic_stop();
+            
+            return 1;
+        }
+    }
+    _iic_stop();
+    
+    return 0;
+} 
+
+/**
+ * @brief      iic bus read
+ * @param[in]  addr is iic device write address
+ * @param[in]  reg is iic register address
+ * @param[out] *buf points to a data buffer
+ * @param[in]  len is the length of the data buffer
+ * @return     status code
+ *             - 0 success
+ *             - 1 read failed
+ * @note       SCL is PE7 and SDA is PE8
+ */
+uint8_t iic_read(uint8_t addr, uint8_t reg, uint8_t *buf, uint16_t len)
+{ 
+    _iic_start();
+    _iic_send_byte(addr);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    _iic_send_byte(reg);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    _iic_start();
+    _iic_send_byte(addr+1);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    
+    while (len)
+    {
+        if (len == 1)
+        {
+            *buf = _iic_read_byte(0);
+        }
+        else
+        {
+            *buf = _iic_read_byte(1);
+        }
+        len--;
+        buf++;
+    }
+    _iic_stop();
+    
+    return 0;
+}
+
+/**
+ * @brief      iic bus read with 16 bits register address 
+ * @param[in]  addr is iic device write address
+ * @param[in]  reg is iic register address
+ * @param[out] *buf points to a data buffer
+ * @param[in]  len is the length of the data buffer
+ * @return     status code
+ *             - 0 success
+ *             - 1 read failed
+ * @note       SCL is PE7 and SDA is PE8
+ */
+uint8_t iic_read_address16(uint8_t addr, uint16_t reg, uint8_t *buf, uint16_t len)
+{ 
+    _iic_start();
+    _iic_send_byte(addr);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    _iic_send_byte((reg>>8)&0xFF);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }    
+    _iic_send_byte(reg&0xFF);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    _iic_start();
+    _iic_send_byte(addr+1);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    while (len)
+    {
+        if (len == 1)
+        {
+            *buf = _iic_read_byte(0);
+        }
+        else
+        {
+            *buf = _iic_read_byte(1);
+        }
+        len--;
+        buf++;
+    }
+    _iic_stop();
+    
+    return 0;
+}
+
+/**
+ * @brief      iic bus read with 16 bits register address 
+ * @param[in]  addr is iic device write address
+ * @param[in]  reg is iic register address
+ * @param[out] *buf points to a data buffer
+ * @param[in]  len is the length of the data buffer
+ * @return     status code
+ *             - 0 success
+ *             - 1 read failed
+ * @note       SCL is PE7 and SDA is PE8
+ */
+uint8_t iic_read_address16_with_scl(uint8_t addr, uint16_t reg, uint8_t *buf, uint16_t len)
+{
+    uint16_t cnt; 
+    
+    _iic_start();
+    _iic_send_byte(addr);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    _iic_send_byte((reg >> 8) & 0xFF);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }    
+    _iic_send_byte(reg & 0xFF);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    delay_ms(100);
+    cnt = 1000;
+    SCL_IN();
+    while (cnt)
+    {
+        if (READ_SCL == 1)
+        {
+            break;
+        }
+        delay_ms(1);
+        cnt--;
+    }
+    SCL_OUT();
+    if (cnt == 0)
+    {
+        return 1;
+    }
+    _iic_start();
+    _iic_send_byte(addr + 1);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    while (len)
+    {
+        if (len == 1)
+        {
+            *buf = _iic_read_byte(0);
+        }
+        else
+        {
+            *buf = _iic_read_byte(1);
+        }
+        len--;
+        buf++;
+    }
+    _iic_stop();
+    
+    return 0;
+}
+
+/**
+ * @brief      iic bus read command
+ * @param[in]  addr is iic device write address
+ * @param[out] *buf points to a data buffer
+ * @param[in]  len is the length of the data buffer
+ * @return     status code
+ *             - 0 success
+ *             - 1 read failed
+ * @note       SCL is PE7 and SDA is PE8
+ */
+uint8_t iic_read_cmd(uint8_t addr, uint8_t *buf, uint16_t len)
+{ 
+    _iic_start();
+    _iic_send_byte(addr + 1);
+    if (_iic_wait_ack())
+    {
+        _iic_stop();
+        
+        return 1;
+    }
+    while (len)
+    {
+        if (len == 1)
+        {
+            *buf = _iic_read_byte(0);
+        }
+        else
+        {
+            *buf = _iic_read_byte(1); 
+        }
+        len--;
+        buf++;
+    }
+    _iic_stop(); 
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Hardware/iic.h
+++ Hardware/iic.h
@@ -4,6 +4,8 @@
 #include "gd32f30x_libopt.h"
 #include <stdlib.h>
 
+/*************************************************Hardware IIC*************************************************/
+
 #define  I2C_SLAVE_ADDRESS7		0x52
 
 typedef uint32_t I2C_TypeDef;
@@ -13,6 +15,130 @@ uint8_t VL6180_Read_Single_Register(I2C_TypeDef I2Cx, uint8_t SlaveAddress, uint
 void VL6180_Write_Single_Register(I2C_TypeDef I2Cx, uint8_t SlaveAddress, uint16_t Reg_Address,uint8_t WriteValue);
 uint8_t VL6180_Write_Multiple_Register(I2C_TypeDef I2Cx, uint8_t SlaveAddress, uint8_t *pBuffer,uint8_t NumByteToWrite);
 uint8_t VL6180_Read_Multiple_Register(I2C_TypeDef I2Cx, uint8_t SlaveAddress, uint8_t *pBuffer,uint8_t NumByteToRead);
+
+/*************************************************Software IIC*************************************************/
+
+
+/**
+ * @defgroup iic iic function
+ * @brief    iic function modules
+ * @{
+ */
+
+/**
+ * @brief  iic bus init
+ * @return status code
+ *         - 0 success
+ * @note   SCL is PE7 and SDA is PE8
+ */
+uint8_t software_iic_init(void);
+
+/**
+ * @brief  iic bus deinit
+ * @return status code
+ *         - 0 success
+ * @note   SCL is PE7 and SDA is PE8
+ */
+uint8_t software_iic_deinit(void);
+
+/**
+ * @brief     iic bus write command
+ * @param[in] addr is iic device write address
+ * @param[in] *buf points to a data buffer
+ * @param[in] len is the length of the data buffer
+ * @return    status code
+ *            - 0 success
+ *            - 1 write failed
+ * @note      SCL is PE7 and SDA is PE8
+ */
+uint8_t iic_write_cmd(uint8_t addr, uint8_t *buf, uint16_t len);
+
+/**
+ * @brief     iic bus write
+ * @param[in] addr is iic device write address
+ * @param[in] reg is iic register address
+ * @param[in] *buf points to a data buffer
+ * @param[in] len is the length of the data buffer
+ * @return    status code
+ *            - 0 success
+ *            - 1 write failed
+ * @note      SCL is PE7 and SDA is PE8
+ */
+uint8_t iic_write(uint8_t addr, uint8_t reg, uint8_t *buf, uint16_t len);
+
+/**
+ * @brief     iic bus write with 16 bits register address 
+ * @param[in] addr is iic device write address
+ * @param[in] reg is iic register address
+ * @param[in] *buf points to a data buffer
+ * @param[in] len is the length of the data buffer
+ * @return    status code
+ *            - 0 success
+ *            - 1 write failed
+ * @note      SCL is PE7 and SDA is PE8
+ */
+uint8_t iic_write_address16(uint8_t addr, uint16_t reg, uint8_t *buf, uint16_t len);
+
+/**
+ * @brief      iic bus read command
+ * @param[in]  addr is iic device write address
+ * @param[out] *buf points to a data buffer
+ * @param[in]  len is the length of the data buffer
+ * @return     status code
+ *             - 0 success
+ *             - 1 read failed
+ * @note       SCL is PE7 and SDA is PE8
+ */
+uint8_t iic_read_cmd(uint8_t addr, uint8_t *buf, uint16_t len);
+
+/**
+ * @brief      iic bus read
+ * @param[in]  addr is iic device write address
+ * @param[in]  reg is iic register address
+ * @param[out] *buf points to a data buffer
+ * @param[in]  len is the length of the data buffer
+ * @return     status code
+ *             - 0 success
+ *             - 1 read failed
+ * @note       SCL is PE7 and SDA is PE8
+ */
+uint8_t iic_read(uint8_t addr, uint8_t reg, uint8_t *buf, uint16_t len);
+
+/**
+ * @brief      iic bus read with 16 bits register address 
+ * @param[in]  addr is iic device write address
+ * @param[in]  reg is iic register address
+ * @param[out] *buf points to a data buffer
+ * @param[in]  len is the length of the data buffer
+ * @return     status code
+ *             - 0 success
+ *             - 1 read failed
+ * @note       SCL is PE7 and SDA is PE8
+ */
+uint8_t iic_read_address16(uint8_t addr, uint16_t reg, uint8_t *buf, uint16_t len);
+
+/**
+ * @brief      iic bus read with 16 bits register address 
+ * @param[in]  addr is iic device write address
+ * @param[in]  reg is iic register address
+ * @param[out] *buf points to a data buffer
+ * @param[in]  len is the length of the data buffer
+ * @return     status code
+ *             - 0 success
+ *             - 1 read failed
+ * @note       SCL is PE7 and SDA is PE8
+ */
+uint8_t iic_read_address16_with_scl(uint8_t addr, uint16_t reg, uint8_t *buf, uint16_t len);
+
+/**
+ * @}
+ */
+
+
+
+
+
+
 
 
 #endif
