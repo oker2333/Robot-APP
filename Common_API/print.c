57cd994b55fab040b00788f9a1499d5554adcb83
2023181377@qq.com <joker>
2022-11-26 19:50:43 +0800

    [优化]重写robot_print

--- Common_API/print.c
+++ Common_API/print.c
@@ -1,8 +1,16 @@
-#include "print.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include "print.h"
 #include "fifo.h"
 #include "app_config.h"
 #include "gd32f30x_libopt.h"
+
+
+#define logENTER_CRITICAL_FROM_ISR() 	uint32_t ulReturn = taskENTER_CRITICAL_FROM_ISR()
+#define logEXIT_CRITICAL_FROM_ISR() 	taskEXIT_CRITICAL_FROM_ISR(ulReturn)
 
 int fputc(int ch, FILE *f)
 {
@@ -14,6 +22,33 @@ int fputc(int ch, FILE *f)
 		usart_data_transmit(USART0, ch);
 		#endif
 	  return 0;
+}
+
+#define LOG_MAX_LEN 256
+
+int robot_print(const char *const fmt, ...)
+{
+	  logENTER_CRITICAL_FROM_ISR();
+	  
+	  uint8_t len;
+    char log_str[LOG_MAX_LEN];
+
+  	va_list args;
+    va_start(args, fmt);
+    len = vsnprintf((char *)log_str, LOG_MAX_LEN, (char const *)fmt, args);	  
+    va_end(args);
+	  
+	  for(int i = 0;i <= len;i++)
+	  {
+			#if FIFO_DEBUG
+			FIFO_Put(Queue_log,ch);
+			#else
+			while(RESET == usart_flag_get(USART0, USART_FLAG_TBE));
+			usart_data_transmit(USART0, log_str[i]);
+			#endif
+		}
+		
+		logEXIT_CRITICAL_FROM_ISR();
 }
 
 #if FIFO_DEBUG
